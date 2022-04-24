faaae45b5bc7f23bcb622b44043eb0fb4b8f30eb
Nick McMillen <nick.mcmillen@student.awesomeinc.org>
2022-04-24 23:32:51 +0000

    intial post layout nearly complete. a. need to adjust avatar name spacing and add likes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import HomeNavbar from "./components/HomeNavbar";
+import PostLayout from './components/PostLayout';
 
 function App() {
   return (
     <div>
       <HomeNavbar />
+      <PostLayout />
     </div>
   );
 }
---
+++ src/components/PostLayout.js
@@ -0,0 +1,62 @@
+import React from "react";
+import "./PostLayout.css";
+import { Button, Card, Col, Image, Row } from "react-bootstrap";
+
+export default function PostLayout() {
+  let username = "username.user";
+  let postdesc = "Some quick example text to build on the card title and make up the bulk of the card's content.";
+
+  return (
+    <>
+      <Card className="mx-auto mt-4" id="post">
+        <Row className="p-0 m-2">
+          <Col className="p-0" auto={4}>
+            <Image
+              className="avatar"
+              roundedCircle
+              src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+            />
+            <strong>{username}</strong>
+          </Col>
+          <Col
+            className="timestamp text-muted p-0"
+            auto={{ span: 4, offset: 4 }}
+          >
+            <Card.Text>{Date.now()}</Card.Text>
+          </Col>
+        </Row>
+        <Card.Img
+          variant="top"
+          src="https://images.unsplash.com/photo-1533418264835-9871c7c2dbf0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmFjZWNhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+        />
+        <Card.Body className="p-0 m-2">
+          <Card.Text>
+            <strong>{username}</strong> {postdesc}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card className="mx-auto mt-4" id="post">
+        <Row className="p-0 m-2">
+          <Col className="p-0" auto={4}>
+            <strong>{username}</strong>
+          </Col>
+          <Col
+            className="timestamp text-muted p-0"
+            auto={{ span: 4, offset: 4 }}
+          >
+            <Card.Text>4 hours ago</Card.Text>
+          </Col>
+        </Row>
+        <Card.Img
+          variant="top"
+          src="https://images.unsplash.com/photo-1533418264835-9871c7c2dbf0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmFjZWNhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+        />
+        <Card.Body className="p-0 m-2">
+          <Card.Text>
+            <strong>{username}</strong> {postdesc}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </>
+  );
+}
