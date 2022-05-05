1a0d9cdef2dd8eb24e8a25bb1fce38951196b0ec
Nick McMillen <nick.mcmillen@student.awesomeinc.org>
2022-05-05 16:50:02 +0000

    user on myprofile now can see only their posts

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -7,16 +7,19 @@ import "./MyProfile.css";
 
 export default function MyProfile() {
   const [state, dispatch] = useGlobalState();
-  const [ posts, setPosts ] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-  useEffect (() => {
-    getData()
-    .then((data) => {
-      setPosts(data)
-    })
-  }, [])
+  useEffect(() => {
+    getData().then((data) => {
+      setPosts(data);
+    });
+  }, []);
 
-  
+  let userPosts = posts.filter(
+    (displayPosts) => displayPosts.created_by.id == state.currentUser.user_id
+  );
+
+  console.log("show user posts", userPosts);
 
   return (
     <>
@@ -56,36 +59,41 @@ export default function MyProfile() {
 
       {/* User's posts */}
       <div>
-      {posts.map(post => 
-      <Card className="mx-auto mt-4" id="post">
-        <Row className="p-0 m-2 post-header">
-          <Col className="p-0" xs={7}>
-            <Image
-              className="avatar ml-1"
-              roundedCircle
-              src={post.created_by.avatar}
+      {/* <Row xs={1} md={2} className="g-4"> */}
+        {userPosts.map((post) => (
+          <Card className="mx-auto mt-4" id="post">
+            <Row className="p-0 m-2 post-header">
+              <Col className="p-0" xs={7}>
+                <Image
+                  className="avatar ml-1"
+                  roundedCircle
+                  src={post.created_by.avatar}
+                />
+                <strong>{post.created_by.username}</strong>
+              </Col>
+              <Col
+                className="timestamp text-muted p-0"
+                xs={{ span: 4, offset: 1 }}
+              >
+                <Card.Text>{post.created_date}</Card.Text>
+              </Col>
+            </Row>
+            <Card.Img
+              variant="top"
+              src={post.photos[0].images}
+              // src="https://images.unsplash.com/photo-1533418264835-9871c7c2dbf0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmFjZWNhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
             />
-            <strong>{post.created_by.username}</strong>
-          </Col>
-          <Col
-            className="timestamp text-muted p-0"
-            xs={{span: 4, offset: 1 }}
-          >
-            <Card.Text>{post.created_date}</Card.Text>
-          </Col>
-        </Row>
-        <Card.Img
-          variant="top"
-          src={post.photos[0].images}
-          // src="https://images.unsplash.com/photo-1533418264835-9871c7c2dbf0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmFjZWNhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-        />
-        <Card.Body className="p-0 m-2">
-          <Card.Text className="m-0">{post.number_of_likes} likes</Card.Text>
-          <Card.Text>
-            <strong>{post.created_by.username}</strong> {post.description}
-          </Card.Text>
-        </Card.Body>
-      </Card>)}
+            <Card.Body className="p-0 m-2">
+              <Card.Text className="m-0">
+                {post.number_of_likes} likes
+              </Card.Text>
+              <Card.Text>
+                <strong>{post.created_by.username}</strong> {post.description}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
+        {/* </Row> */}
       </div>
     </>
   );
