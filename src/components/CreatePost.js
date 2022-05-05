4a31bcebbfa7a0ec66a448c02d831d711928d573
Nick McMillen <nick.mcmillen@student.awesomeinc.org>
2022-05-05 21:45:49 +0000

    changed d request const in api.requests to not overwrite headers still sending empty form data

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -13,14 +13,21 @@ export default function CreatePost() {
   const [state, dispatch] = useGlobalState();
 
   // set the post state to empty information with current user
+  // const [post, setPost] = useState({
+  //   created_by: state.currentUser.user_id,
+  //   description: "",
+  //   image: "",
+  // });
+  // const [image, setImage] = useState()
   const [post, setPost] = useState({
     created_by: state.currentUser.user_id,
     description: "",
-    image: "",
+    image: ""
   });
 
   // able to check 'post' state in the console. working when adding post info 5/4 at 9pm
   console.log(post);
+  // console.log(image);
   // console.log(JSON.stringify(post))
   // console.log('create post user test', state.currentUser.user_id)
 
@@ -31,69 +38,66 @@ export default function CreatePost() {
     });
   };
 
-  // create the function to set the post
-  // const handleCreatePost = async (e) => {
-  //   // e.preventDefault();
-  //   console.log('create post button pushed')
-  //   // do I need to add a post method in authservice???
-  //   let options = {
-  //     url: '/api/posts/',
-  //     method: 'POST'
-
-  //   }
-  // }
-
-  // const handleCreatePost = () => {
-  //   axios({
-  //     method: "POST",
-  //     url: "https://8000-nmcmillen-ocularbackend-sm1tv8tjiev.ws-us44.gitpod.io/api/posts/",
-  //     headers: {
-
-  //       Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjUxNzU3Mjg2LCJpYXQiOjE2NTE3NTM2ODYsImp0aSI6IjE0ZTczZTU5YTJmMzRlZDM4ZWNkOTFkOWUwNjY0NTE1IiwidXNlcl9pZCI6Mn0.QqNj_tGQsq4t2eu64LIpyZOM9I0SfcWRCVP665qBGeg"
-  //     },
-  //     post
-  //   })
-  //     .then((res) => console.log(res))
-  //     .catch((err) => console.log(err));
+  // const handleImage = (key, value) => {
+  //   setImage({
+  //     ...image,
+  //     [key]: value,
+  //   });
   // };
 
-  // console.log(state.currentUser.access)
-  // console.log(Userfront.accessToken())
-  // console.log('get user token', JSON.parse(localStorage.getItem('user'))['access_token'])
+  // const handleCreatePost = async (e) => {
+  //   e.preventDefault();
+  //   let newPost = new FormData();
+  //   newPost.append("created_by", post.created_by);
+  //   newPost.append("description", post.description);
+  //   // newPost.append("image", post.image);
+  //   let resp = await request({
+  //     url: "api/posts/",
+  //     method: "POST",
+  //     // data: newPost,
+  //     body: newPost,
+  //     headers: { "Content-Type": "multipart/form-data"}
+  //   });
+  //   console.log(resp)
+  // };
+
+  const handleCreatePost = async () => {
+    console.log('test', post, image)
+    let newPost = new FormData();
+    newPost.append("created_by", post.created_by);
+    newPost.append("description", post.description);
+    newPost.append("image", post.image);
+    // newPost.append("image", image); working
+
+    console.log("posty", newPost)
+
+    let res = await request({
+      url: 'api/posts/',
+      method: 'POST',
+      body: newPost,
+      headers: { 'Content-Type': 'multipart/form-data' }
+    })
+    console.log("post again", newPost)
+    // fetch('https://8000-nmcmillen-ocularbackend-sm1tv8tjiev.ws-us44.gitpod.io/api/posts/', {
+    //   method: 'POST',
+    //   body: newPost
+    // })
+    console.log('test res', res)
+    .then( res => console.log(res))
+    .catch( error => console.log(error))
+  }
+
 
   
   // ##THIS SENDS DATA BUT GETS 401 ERROR ##
-  const handleCreatePost = async () => {
-    let resp = await request({
-      url: 'api/posts/',
-      method: 'POST',
-      data: post
-    })
-    console.log(resp)
-    
-    // let token =
-    //   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjUxNzY0MzU4LCJpYXQiOjE2NTE3NjA3NTgsImp0aSI6IjEzMDU4MjQ1ZGVjMTRmOTQ5NjdhNjc1NDY3ZDBkMWE3IiwidXNlcl9pZCI6Mn0.QyX-fwRjuP9PLvHOJEpDmt0GYtgmwzcWG3glb9oDLdg";
-
-    // fetch(
-    //   "https://8000-nmcmillen-ocularbackend-sm1tv8tjiev.ws-us44.gitpod.io/api/posts/",
-    //   {
-    //     method: "POST",
-    //     body: post, //tried to stringify 'post' info but still doesn't work 
-    //     headers: {
-    //       "Content-Type": "application/json", //without this in it gets a 415 error related to media/content type
-    //       // 'Content-Type': 'multipart/form-data',
-    //       Authorization: `Bearer ${token}`,
-    //     },
-    //   }
-    // )
-    //   .then((response) => response.json)
-    //   .then((result) => {
-    //     console.log("Success:", result);
-    //   })
-    //   .catch((error) => {
-    //     console.error("Error:", error);
-    //   });
-  };
+  // const handleCreatePost = async () => {
+  //   let resp = await request({
+  //     url: 'api/posts/',
+  //     method: 'POST',
+  //     data: post, image
+  //   })
+  //   console.log(resp)
+  // };
 
   return (
     <>
@@ -143,3 +147,63 @@ export default function CreatePost() {
     </>
   );
 }
+
+
+
+    // let token =
+    //   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjUxNzY0MzU4LCJpYXQiOjE2NTE3NjA3NTgsImp0aSI6IjEzMDU4MjQ1ZGVjMTRmOTQ5NjdhNjc1NDY3ZDBkMWE3IiwidXNlcl9pZCI6Mn0.QyX-fwRjuP9PLvHOJEpDmt0GYtgmwzcWG3glb9oDLdg";
+
+    // fetch(
+    //   "https://8000-nmcmillen-ocularbackend-sm1tv8tjiev.ws-us44.gitpod.io/api/posts/",
+    //   {
+    //     method: "POST",
+    //     body: post, //tried to stringify 'post' info but still doesn't work 
+    //     headers: {
+    //       "Content-Type": "application/json", //without this in it gets a 415 error related to media/content type
+    //       // 'Content-Type': 'multipart/form-data',
+    //       Authorization: `Bearer ${token}`,
+    //     },
+    //   }
+    // )
+    //   .then((response) => response.json)
+    //   .then((result) => {
+    //     console.log("Success:", result);
+    //   })
+    //   .catch((error) => {
+    //     console.error("Error:", error);
+    //   });
+
+
+
+
+
+
+  // create the function to set the post
+  // const handleCreatePost = async (e) => {
+  //   // e.preventDefault();
+  //   console.log('create post button pushed')
+  //   // do I need to add a post method in authservice???
+  //   let options = {
+  //     url: '/api/posts/',
+  //     method: 'POST'
+
+  //   }
+  // }
+
+  // const handleCreatePost = () => {
+  //   axios({
+  //     method: "POST",
+  //     url: "https://8000-nmcmillen-ocularbackend-sm1tv8tjiev.ws-us44.gitpod.io/api/posts/",
+  //     headers: {
+
+  //       Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjUxNzU3Mjg2LCJpYXQiOjE2NTE3NTM2ODYsImp0aSI6IjE0ZTczZTU5YTJmMzRlZDM4ZWNkOTFkOWUwNjY0NTE1IiwidXNlcl9pZCI6Mn0.QqNj_tGQsq4t2eu64LIpyZOM9I0SfcWRCVP665qBGeg"
+  //     },
+  //     post
+  //   })
+  //     .then((res) => console.log(res))
+  //     .catch((err) => console.log(err));
+  // };
+
+  // console.log(state.currentUser.access)
+  // console.log(Userfront.accessToken())
+  // console.log('get user token', JSON.parse(localStorage.getItem('user'))['access_token'])
