c26513cda9f821f1a803f8f05ad5e32d96606f34
Nick McMillen <nick.mcmillen@student.awesomeinc.org>
2022-05-12 16:52:38 +0000

    changed API_URL with process.env

--- src/components/services/auth.constants.js
+++ src/components/services/auth.constants.js
@@ -1,4 +1,5 @@
-export const API_URL = 'https://8000-nmcmillen-ocularbackend-sm1tv8tjiev.ws-us44.gitpod.io/';
+// export const API_URL = 'https://8000-nmcmillen-ocularbackend-sm1tv8tjiev.ws-us44.gitpod.io/';
+export const API_URL = process.env.REACT_APP_OCULAR_API_URL;
 export const LOGIN_ENDPOINT = "user/login/"
 export const REGISTER_ENDPOINT = "user/signup/"
 export const REFRESH_ENDPOINT = "token/refresh/"
